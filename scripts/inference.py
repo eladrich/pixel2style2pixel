102e17763d288de7b87f850487ddd8de86776d82
yuval-alaluf <yuvalalaluf@gmail.com>
2020-10-20 19:09:59 +0300

    Small fix when opts.n_images is None

--- scripts/inference.py
+++ scripts/inference.py
@@ -63,6 +63,9 @@ def run():
 	                        num_workers=int(opts.test_workers),
 	                        drop_last=True)
 
+	if opts.n_images is None:
+		opts.n_images = len(dataset)
+	
 	global_i = 0
 	global_time = []
 	for input_batch in tqdm(dataloader):
